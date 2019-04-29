fa6aeee8214dbce2ccb0861062b4fe5418215870
Yong-Gwi Cho <yonggwi.cho@gmail.com>
2019-04-29 21:57:29 +0900

    refacter directory structure.
    start to develop fBM simulator.

---
+++ bitcoing/fBM.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy as sp
+import os
+
+
+class fBM:
+    def __init__(self):
+
+    def gamma(self,n):
+
+    def B(self,H,t):
+        # H is Hurst exponent
+        # t is a time point
